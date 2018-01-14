bb8db3efde1a5ae108e36a790c701736f25c1825
KYOKO <git.kyoko@gmail.com>
2018-01-15 08:13:07 +0900

    Create posted.php

---
+++ posted.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>投稿一覧</title>
+</head>
+<body>
+  <main>
+  <form action="#" method="post">
+    <ul>
+      <li><label for="posted">投稿ID</label>
+      <li><label for="contest_id">コンテストID</label>
+      <li><label for="nickname">ニックネーム</label>
+      <li><label for="email">メールアドレス</label>
+      <li><label for="posted_date">投稿日時</label>
+      <li><label for="ditail">詳細</label>
+    </ul>
+  </form>
+  </main>
+</body>
+</html>
